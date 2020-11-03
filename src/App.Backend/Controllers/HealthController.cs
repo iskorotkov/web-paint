1b038e78f79a9e24b456d8826e7e9c9d5074581f
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-11-03 19:53:33 +0500

    feat(backend): add backend

---
+++ src/App.Backend/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace App.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("Health checked.");
+            return "The service is healthy";
+        }
+    }
+}
---
+++ src/App.Backend/Controllers/PluginsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Plugins.Base;
+using Plugins.Config;
+using Plugins.Loader;
+
+namespace App.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PluginsController : ControllerBase
+    {
+        private readonly ILogger<PluginsController> _logger;
+        private readonly PluginLoader _pluginLoader;
+        private readonly ConfigLoader _configLoader;
+        private readonly IConfiguration _configuration;
+
+        public PluginsController(ILogger<PluginsController> logger, PluginLoader pluginLoader,
+            ConfigLoader configLoader, IConfiguration configuration)
+        {
+            _logger = logger;
+            _pluginLoader = pluginLoader;
+            _configLoader = configLoader;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public Dictionary<string, IPlugin> Get()
+        {
+            _logger.LogInformation("Plugins requested.");
+
+            var configPath = _configuration["Plugins:Config"];
+            _logger.LogTrace("Config path is %s", configPath);
+
+            var config = string.IsNullOrEmpty(configPath)
+                ? new PluginsConfig()
+                : _configLoader.Load(configPath);
+
+            _logger.LogInformation("Plugins were successfully loaded.");
+            return _pluginLoader.Load(config);
+        }
+    }
+}
